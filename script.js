fcefb4ba585c9a240173b6855956ec9b43833ce6
Ricellyo Cruz Silva <ricellyocruzsilva@gmail.com.br>
2024-10-07 08:20:27 -0300

    adicionando novas funções e buscando api

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 /* 
 passos :
 -> crindo function, passando valor e tastando.
-->
-->
+-> capturar elemento do INPUT
+-> ir ao servidor e pegar as informações do tempo atualiada.
 
 */ 
 
+let chave = "cebcd482eda57fa9a6714c1c2ba91885"
+
+async function localizarCidade() {
+    let dados = " https://api.openweathermap.org/data/2.5/weather?q=londre&appid=cebcd482eda57fa9a6714c1c2ba91885]";
+
+    console.log(dados);
+}
+
 function clicarNoBotao() {
     let localidade = document.querySelector(".input-cidade").value;
-    console.log(localidade);  
+
+    localizarCidade();
 }
+
+
+
+
+
+
+// minuto 1.46

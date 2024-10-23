6b0f9874fb7dca75f43a9e35cbc7e726e79d100d
Ricellyo Cruz Silva <ricellyocruzsilva@gmail.com.br>
2024-10-23 08:25:26 -0300

    organizando informação atualizada vindo  do servidor

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ passos :
 -> crindo function, passando valor e tastando.
 -> capturar elemento do INPUT
 -> ir ao servidor e pegar as informações do tempo atualiada.
+-> Organizando as informações que chegaram(devtools) coveretendo para grau celso.
 
 */ 
 
@@ -10,14 +11,17 @@ passos :
 
 let chave = "8bfeb89b42e07862ad31b245161b34a1";
 
-async function localizarCidade() {
-    let dados = await fetch("https://api.openweathermap.org/data/2.5/weather?q=londres&appid=8bfeb89b42e07862ad31b245161b34a1").then ( resposta => resposta.json());
+async function localizarCidade(localidade) {
+    let dados = await fetch("https://api.openweathermap.org/data/2.5/weather?q="+ localidade + "&appid=8bfeb89b42e07862ad31b245161b34a1&units=metric").then ( resposta => resposta.json());
 
     // ASYNC = espere
     // AWAIT = ferramenta js p/ acessar servidores
     // FETCH = Ferramenta JS para acessar servidores
     // THEN = Então
     // JSON = Java Script object Notation (formata que js entende ).
+    // & = separar
+    // "units=metric" = coverter para grau celcios  ex: &units=metric
+        
 
     console.log(dados);
 }
@@ -25,7 +29,7 @@ async function localizarCidade() {
 function clicarNoBotao() {
     let localidade = document.querySelector(".input-cidade").value;
 
-    localizarCidade();
+    localizarCidade(localidade);
 }
 
 

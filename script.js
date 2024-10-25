4cc830889b97b0bd1061222196f490f4ffdbc3b1
Ricellyo Cruz Silva <ricellyocruzsilva@gmail.com.br>
2024-10-25 08:24:04 -0300

    adicionando uma classe cidade e capturando elemento para mostrar cidade e tempo real na tela

--- script.js
+++ script.js
@@ -9,7 +9,13 @@ passos :
 
 // let chave = "cebcd482eda57fa9a6714c1c2ba91885"
 
+
 let chave = "8bfeb89b42e07862ad31b245161b34a1";
+
+function colocarNaTela(dados){
+    console.log(dados);
+    document.querySelector(".cidade").innerHTML = dados.name
+}
 
 async function localizarCidade(localidade) {
     let dados = await fetch("https://api.openweathermap.org/data/2.5/weather?q="+ localidade + "&appid=8bfeb89b42e07862ad31b245161b34a1&units=metric").then ( resposta => resposta.json());
@@ -23,7 +29,7 @@ async function localizarCidade(localidade) {
     // "units=metric" = coverter para grau celcios  ex: &units=metric
         
 
-    console.log(dados);
+    colocarNaTela(dados)
 }
 
 function clicarNoBotao() {
